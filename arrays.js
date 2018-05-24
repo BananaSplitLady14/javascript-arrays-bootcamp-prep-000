da6f64269f8f6ca333fb71a09dcbaaf35688604b
Sam Santiago <supergirl78.dg@gmail.com>
2018-05-24 23:05:29 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var grilledCheeseIngredients = [
+'bread',
+'mild cheese',
+'sharp cheese',
+'butter',
+'tomato',
+'garlic'
+];
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+];
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles',
+];
+
+function addElementToBeginningOfArray () {
+
+}
+
+function destructivelyAddElementToBeginningOfArray () {
+
+}
